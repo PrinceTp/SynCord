eb382a4e2e6f5dc6d04c92bad0d45ed31439549c
PrinceTp <tprince1812@gmail.com>
2023-12-15 22:38:21 +0530

    Init

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+import {cn} from "@/lib/utils";
+
+const state = true;
+
+export default function Home() {
+  return (
+    <div>
+    <p className="text-3xl font-bold text-indigo-500">
+      SynCord,
+      Hello.
+    </p>
+    <Button>
+      Click me
+    </Button>
+    </div>
+  )
+}
