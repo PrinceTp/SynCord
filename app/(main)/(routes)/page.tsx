5aa285f058fd2f07c31681b26e4f7bcac862dd5f
PrinceTp <tprince1812@gmail.com>
2023-12-15 23:48:35 +0530

    Authentication_Clerk

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,13 +1,15 @@
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 import {cn} from "@/lib/utils";
+import { UserButton } from '@clerk/nextjs';
 
 const state = true;
 
 export default function Home() {
   return (
-    <div>
-    This is a protected page
-    </div>
+     <div className="h-screen">
+       <UserButton afterSignOutUrl="/"
+      />
+     </div>
   )
 }
